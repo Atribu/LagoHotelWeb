36357635467a6848e3c83df4874c48905819eb05
Ece Bayramer <ecebayramer20@gmail.com>
2025-02-05 16:30:42 +0300

    subroom features

--- client/app/rooms/subroom/page.js
+++ client/app/rooms/subroom/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SubRoomBanner from './components/SubRoomBanner'
 import ContactSection from '@/app/GeneralComponents/Contact/ContactSection'
 import SubroomCarousel from './components/SubroomCarousel'
+import RoomFeatures from './components/RoomFeatures'
 
 const page = () => {
   return (
     <div className='flex flex-col bg-white'>
       <SubRoomBanner/>
       <SubroomCarousel/>
+      <RoomFeatures/>
       <div className='my-10'></div>
       <ContactSection/>
     </div>
