c0c964f3f7584448e4805654dc5c4335079fa38f
Atribu <oguzkilinc.ant@gmail.com>
2025-02-05 17:09:59 +0300

    SliderUpdate1
    SliderUpdate1

--- client/app/HomePage/HomePage.jsx
+++ client/app/HomePage/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import HomePage1 from "./Components/HomePage"
 import Reservation from './Components/Reservation'
 import HomePage2 from "./Components/HomePage1"
+import HomePage3 from "./Components/HomePage2"
+import HomePage4 from "./Components/HomePage3"
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
         <HomePage1 />
         <Reservation/>
         <HomePage2 />
+        <HomePage3 />
+        <HomePage4 />
     </div>
   )
 }
