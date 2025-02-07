098f5215de8410fcf40e75eb1e32e32d6a8f700f
Ece Bayramer <ecebayramer20@gmail.com>
2025-02-07 11:06:40 +0300

    kids carousel

--- client/app/kidsclub/page.js
+++ client/app/kidsclub/page.js
@@ -3,13 +3,15 @@ import MainBannerSection from '../GeneralComponents/MainBannerSection'
 import image from "./images/mainkids.webp"
 import KidsBamboo from './components/KidsBamboo'
 import KidsIconsSection from './components/KidsIconsSection'
+import KidsclubCarousel from './components/KidsclubCarousel'
 
 const page = () => {
   return (
-    <div>
+    <div className='overflow-hidden'>
       <MainBannerSection img={image}/>
       <KidsBamboo/>
       <KidsIconsSection/>
+      <KidsclubCarousel/>
     </div>
   )
 }
