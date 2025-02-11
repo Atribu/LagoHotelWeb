5e936c6c83f7ba532ee47d7ba844c06e88788a3a
Atribu <oguzkilinc.ant@gmail.com>
2025-02-11 18:13:45 +0300

    beachguncel

---
+++ client/app/beachpools/Components/Beach2.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import foto from "../Images/beachfull.webp"
+import BirthDay from "../Images/Icons/BirthDay"
+import Baloon from "../Images/Icons/Baloon"
+
+const Beach2 = () => {
+  return (
+    <div 
+      className="flex flex-col w-full mt-20 h-[600px] bg-cover bg-center" 
+      style={{ backgroundImage: `url(${foto.src})` }}
+    >
+      {/* Metin Bloğu: Siyah arka plan, %50 opaklık; genişlik %33 olacak şekilde ayarlandı */}
+      <div className="flex flex-col items-start justify-center h-full w-5/12 gap-4 p-4 bg-black bg-opacity-50 text-white">
+        <p className='font-jost text-[12px] font-medium leading-[14px] tracking-[0.48px] uppercase ml-12'>
+          Cabana and Pavilion Rentals
+        </p>
+        <h3 className='font-marcellus text-[40px] font-normal leading-[57.6px] ml-12'>
+          Relax in Seaside <br /> Luxury
+        </h3>
+        <p className='font-jost text-[16px] font-normal leading-[24px] ml-12'>
+          Unwind in our exclusive Cabanas and Pavilions, available for a fee, <br/> offering unmatched  privacy and comfort by the beach. Guests staying <br/> in Swim-Up Rooms enjoy special discounts, making your seaside <br/> experience even more luxurious.
+        </p>
+        <span className='font-marcellus text-[20px] font-normal leading-[24px] tracking-[-0.4px] ml-12'>
+          Special Set-up Available
+        </span>
+        {/* İkonlar ve Yanlarındaki Yazılar */}
+        <div className="flex w-5/6 gap-4 p-2 text-white justify-center items-center ml-12">
+          <div className="flex items-center w-1/2 gap-2 rounded">
+            <BirthDay className="text-2xl" width={35} height={35} />
+            <span>Icon One Text</span>
+          </div>
+          <div className="flex items-center w-1/2 gap-2 p-1 rounded">
+            <Baloon className="text-2xl" width={35} height={35} />
+            <span>Icon Two Text</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Beach2
