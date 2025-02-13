c5e8bde5ecad556fede525bff9405dffad202385
Ece Bayramer <ecebayramer20@gmail.com>
2025-02-13 11:14:01 +0300

    activities edited

--- client/app/restaurants/components/ClinaryInfoSection.jsx
+++ client/app/restaurants/components/ClinaryInfoSection.jsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 const ClinaryInfoSection = ({img1,img2,span,header,texts=[]}) => {
   return (
     <div className="flex w-screen h-[555px] my-[100px] items-center justify-center">
-      <div className="flex w-[76.8%] items-center justify-between ml-32 h-full">
+      <div className="flex w-[76.8%] items-center justify-center gap-[52px] h-full">
 
-        <div className="flex flex-col w-[47%] items-start justify-center text-start gap-[35.07px] text-black font-jost">
+        <div className="flex flex-col w-[49%] items-start justify-center text-start gap-[35.07px] text-black font-jost">
           <span className="text-[12.002px] font-medium leading-[14.026px] tracking-[0.481px] uppercase">
           {span}
           </span>
@@ -32,23 +32,11 @@ const ClinaryInfoSection = ({img1,img2,span,header,texts=[]}) => {
               ))}
             </ul>
           )}
-          {/* <p className="text-[16.03px] font-normal leading-[24.045px] leading-trim-both text-edge-cap">
-            LAGO, you will enjoy your vacation with international cuisine in our
-            A’la Carte Restaurants, where flavorful dishes are served, with an
-            endless open buffet where delicious dishes feature uniqueness from
-            Turkish cuisine as well as fresh flavors reflecting the spirit of
-            the Mediterranean
-          </p>
-          <ul className="text-[16.03px] font-normal leading-[24.045px] list-disc pl-5 marker:text-xs marker:text-black">
-           <li> Ana Restoran: Kahvaltı, Öğlen ve Akşam Yemeği </li>
-          <li>  Bistro Restoran: 24 Saat</li>
-           <li> Snack Bars: 24 Saat</li>
-          </ul> */}
         </div>
 
-        <div className="flex w-[49%] items-start justify-start relative">
+        <div className="flex w-[48%] items-start justify-start relative h-[555px]">
             <Image src={img2} alt="art" width={img2.width} height={img2.height}/>
-            <Image src={img1} alt="art" width={img1.width} height={img1.height} className="absolute top-[24%] left-[30%]"/>
+            <Image src={img1} alt="art" width={img1.width} height={img1.height} className="absolute top-[105px] left-[215px]"/>
         </div>
 
       </div>
