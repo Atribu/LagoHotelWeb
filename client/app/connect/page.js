0298f97d1e1d429b984339cd4956ee05b1b2a9c9
Atribu <oguzkilinc.ant@gmail.com>
2025-02-14 09:54:33 +0300

    v4

--- client/app/connect/page.js
+++ client/app/connect/page.js
@@ -1,37 +1,13 @@
 import React from 'react'
+import Connect1 from "./components/Connect1.jsx"
+import Connect2 from './components/Connect2.jsx'
 
 const page = () => {
   return (
     <div>
-        <div>
-            resim olacak resim üstünde yazılar olacak
-        </div>
-        <div>
-            <div>
-                icon ve Our Address:
-                <p>
-                  Sorgun, Titreyengöl Mevkii 07600 Manavgat/Antalya
-                </p>
-            </div>
-            <div className='bg-black h-[30px] w-[1px]' />
-            <div>
-                icon ve Phone Number:
-                <p>
-                  +90 242 756 99 00
-                  +90 242 524 57 87
-                </p>
-            </div>
-            <div className='bg-black h-[30px] w-[1px]' />
-            <div>
-                icon ve Email Adress:
-                <p>
-                  sales@lagohotel.com
-                  info@lagohotel.com
-                </p>
-            </div>
-        </div>
+      <Connect1 />
+      <Connect2 />
     </div>
-
   )
 }
 
