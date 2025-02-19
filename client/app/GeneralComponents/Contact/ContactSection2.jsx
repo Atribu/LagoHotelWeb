3b547d2ba310a0e5f6f8c0b81f446f23b86afd42
Ece Bayramer <ecebayramer20@gmail.com>
2025-02-19 11:02:01 +0300

    footer and contact

--- client/app/GeneralComponents/Contact/ContactSection2.jsx
+++ client/app/GeneralComponents/Contact/ContactSection2.jsx
@@ -7,11 +7,12 @@ import minigallery from "./images/minigallery.webp"
 
 const ContactDetails = () => {
   return (
-    <div className="flex flex-col justify-start items-start w-5/12 ml-[4%] gap-5">
-      <span className="font-jost ml-[2%] w-7/12 text-[12px] font-medium leading-[14px] tracking-[0.48px] uppercase">
+    <div className="flex flex-col justify-center items-center w-[40%] ">
+      <div className='flex flex-col w-[313px] items-start justify-center gap-[35px]'>
+      <span className="font-jost text-[12px] font-medium leading-[14px] tracking-[0.48px] uppercase">
         CONTACT US FOR MORE
       </span>
-      <h2 className="font-marcellus text-[48px] leading-[57.6px] mr-[5%]">@LagoHotels</h2>
+      <h2 className="font-marcellus text-[48px] leading-[57.6px] capsizedText2">@LagoHotels</h2>
       <p className="font-jost text-[16px] leading-[24px] underline underline-offset-2 flex flex-col gap-2">
         <span>Titreyengol, Sorgun / Manavgat/ Antalya / TR</span>
         <span>Phone: +90 242 756 99 00</span>
@@ -29,14 +30,15 @@ const ContactDetails = () => {
           BOOK NOW
         </span>
       </div>
+      </div>
     </div>
   )
 }
 
 const GallerySection = () => {
   return (
-    <div className="flex justify-end items-end">
-      <div className="w-9/12 h-[400px] overflow-y-auto scroll-smooth mr-[4%]">
+    <div className="flex justify-end items-center w-[60%]">
+      <div className="w-full h-[27vh] min-h-[322px] max-w-[789px] overflow-y-auto scroll-smooth mr-[4%]">
         <Image src={minigallery} height={700} width={1200} alt="Minigallery" />
       </div>
     </div>
@@ -45,8 +47,8 @@ const GallerySection = () => {
 
 const ContactSection2 = () => {
   return (
-    <div className="flex w-full h-[50vh] max-w-[1444px] mb-[100px]">
-      <div className="flex w-11/12 flex-row justify-center items-center border-[1px] border-black">
+    <div className="flex w-screen  max-w-[1440px] mb-[100px] justify-start ">
+      <div className="flex justify-center items-center border-[1px] border-lagoBlack min-w-[1401px] h-[35vh] min-h-[422px] ">
         <ContactDetails />
         <GallerySection />
       </div>
--- client/app/GeneralComponents/Footer/Footer.jsx
+++ client/app/GeneralComponents/Footer/Footer.jsx
@@ -7,79 +7,82 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#1A1A16] text-gray-200 text-sm py-16 flex justify-center z-10">
-      <div className="w-full px-44">
-        <div className="grid grid-cols-5 gap-12">
+    <footer className="w-full flex flex-col bg-[#1A1A16] text-gray-200 text-sm justify-center items-center z-10">
+      <div className="flex w-[91.1%] max-w-[1440px] items-start justify-center text-start gap-[4%] mt-[60px]">
+        
+        <div className="flex w-[66%] items-start text-start">
           {/* SOL: 4 SÜTUN (QUICK MENU, TERMS, COMPANY, CONTACT) */}
-          <div className="col-span-4 grid grid-cols-4 gap-8">
+          <div className="w-full grid grid-cols-4 gap-8">
             {/* QUICK MENU */}
-            <div>
-              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase mb-7">
+            <div className="flex flex-col items-start justify-start text-start gap-[40px]">
+              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase ">
                 QUICK MENU
               </h4>
-              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-4">
-                <li>Home</li>
-                <li>Service</li>
-                <li>Portfolio</li>
-                <li>Product</li>
-                <li><Link href="/news">Blog</Link></li>
-                <li>FAQ</li>
+              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-6">
+                <li className="hover:text-white cursor-pointer">Home</li>
+                <li className="hover:text-white cursor-pointer">Service</li>
+                <li className="hover:text-white cursor-pointer">Portfolio</li>
+                <li className="hover:text-white cursor-pointer">Product</li>
+                <li className="hover:text-white cursor-pointer"><Link href="/news">Blog</Link></li>
+                <li className="hover:text-white cursor-pointer">FAQ</li>
               </ul>
             </div>
 
             {/* TERM & CONDITIONS */}
-            <div>
-              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase mb-7">
+            <div className="flex flex-col items-start justify-start text-start gap-[40px]">
+              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase ">
                 TERM & CONDITIONS
               </h4>
-              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-4">
-                <li>Privacy Policy</li>
-                <li>Bookings & Cancellations</li>
-                <li>Website Terms</li>
-                <li>Product</li>
-                <li><Link href="/news">Blog</Link></li>
-                <li>FAQ</li>
+              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-6">
+                <li className="hover:text-white cursor-pointer">Privacy Policy</li>
+                <li className="hover:text-white cursor-pointer">Bookings & Cancellations</li>
+                <li className="hover:text-white cursor-pointer">Website Terms</li>
+                <li  className="hover:text-white cursor-pointer">Product</li>
+                <li className="hover:text-white cursor-pointer"><Link href="/news">Blog</Link></li>
+                <li className="hover:text-white cursor-pointer">FAQ</li>
               </ul>
             </div>
 
             {/* COMPANY */}
-            <div>
-              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase mb-7">
+            <div className="flex flex-col items-start justify-start text-start gap-[40px]">
+              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase ">
                 COMPANY
               </h4>
-              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-4">
-                <li>About Company</li>
-                <li><Link href="/connect">Contact</Link></li>
-                <li>Terms & Condition</li>
-                <li>Privacy & Policy</li>
-                <li>Help Center</li>
+              <ul className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] space-y-6">
+                <li className="hover:text-white cursor-pointer">About Company</li>
+                <li className="hover:text-white cursor-pointer"><Link href="/connect">Contact</Link></li>
+                <li className="hover:text-white cursor-pointer">Terms & Condition</li>
+                <li className="hover:text-white cursor-pointer">Help Center</li>
               </ul>
             </div>
 
             {/* CONTACT */}
-            <div>
-              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase mb-7">
+            <div className="flex flex-col items-start justify-start text-start gap-[40px]">
+              <h4 className="text-[15px] text-white font-marcellus font-normal leading-[26px] tracking-[0.6px] uppercase ">
               <Link href="/connect">Contact</Link>
               </h4>
-              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] mb-4">
-                Phone: +90 242 756 99 00
+              <div className="flex flex-col gap-[24px]">
+              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] ">
+                Phone: <span className="underline">+90 242 756 99 00</span>
               </p>
-              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] mb-4">
+              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px]">
                 Email: info@lagohotels.com
               </p>
-              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px] mb-4">
+              <p className="text-lagoGray2 font-jost text-[16px] font-normal leading-[20px]">
                 Address: Titreyengöl
                 <br />
                 Mevkii Sorgun Manavgat/
                 <br />
                 Antalya / TR
               </p>
+              </div>
             </div>
           </div>
 
-          {/* SAĞ: LOGO + SOSYAL İKONLAR + LİNKLER */}
-          {/* Burada border-l => dikey çizgi */}
-          <div className="col-span-1 h-48 mt-4 flex flex-col items-start border-l border-gray-400 pl-6">
+          
+        </div>
+
+        <div className="w-[30%] flex flex-col items-start border-l border-gray-400 pl-16 gap-[40px]">
             {/* Logo */}
             <div className="mb-10">
               <Image
@@ -92,7 +95,7 @@ export default function Footer() {
             </div>
 
             {/* Sosyal ikonlar */}
-            <div className="flex items-center gap-7 mb-4">
+            <div className="flex items-center gap-[32px]">
               <FaInstagram className="w-[30px] h-[32px]" />
               <FaLinkedinIn className="w-[30px] h-[32px]" />
               <FaFacebook className="w-[30px] h-[32px]" />
@@ -100,20 +103,18 @@ export default function Footer() {
             </div>
 
             {/* Alt linkler */}
-            <div className="flex items-center gap-6 whitespace-nowrap mt-6 text-lagoGray2">
+            <div className="flex items-center gap-6 whitespace-nowrap text-lagoGray2">
               <a href="#" className="hover:underline">Terms of Use</a>
               <a href="#" className="hover:underline">Privacy Notice</a>
               <a href="#" className="hover:underline">Sustainability</a>
             </div>
-          </div>
-        </div>
-
-        {/* Alt ince çizgi */}
-        <div className="w-full h-[1px] bg-gray-400 mt-6" />
-        <div className="flex text-center items-center justify-center mt-2">
-            dgtlface
-        </div>
+          </div>        
       </div>
+      {/* Alt ince çizgi */}
+      <div className="w-full h-[1px] bg-gray-400 mt-[60px]" />
+      <div className="flex text-center items-center justify-center text-[15px] font-normal leading-[26px]">
+            Powered by DGTLFACE
+        </div>
     </footer>
   )
 }
