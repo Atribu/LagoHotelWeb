6d28a12502bc3212c710159e599b712c05c977a6
Ece Bayramer <ecebayramer20@gmail.com>
2025-02-27 14:53:33 +0300

    cookie and connect

--- client/app/connect/components/Connect2.jsx
+++ client/app/connect/components/Connect2.jsx
@@ -4,66 +4,68 @@ import image from '../Image/SRF_7996-min.webp'
 
 const Connect2 = () => {
   return (
-    <div className="relative w-full ] flex flex-col items-center">
+    <div className="relative w-full flex flex-col items-center ">
       {/* Arka plan resmi container'ı: genişlik 978px, yükseklik 744px, solda kaydırılmış */}
-      <div className="relative w-[978px] h-[744px] -ml-[15%]">
+      <div className="relative w-full lg:w-[978px] lg:h-[744px] lg:-ml-[15%]">
         <Image
           src={image}
           alt="Resim"
-          fill
-          className="object-cover"
+        
+          width={image.width}
+          height={image.height}
+          className="object-cover w-full min-h-[650px]"
           priority
         />
       </div>
       
       {/* Form overlay */}
-      <div className="absolute w-[480px] h-[684px] top-[4%] right-[20%] bg-white shadow-lg p-6 pt-8 max-w-md z-10">
-        <h2 className="text-3xl md:text-4xl font-normal text-center mb-4 font-marcellus text-[48px] text-lagoBlack leading-[56px] mt-2">
+      <div className="absolute min-w-[350px] w-[78vw] max-w-[480px] min-h-[30vh] md:min-h-[40vh] lg:w-[480px] lg:h-[684px] top-1/2  -translate-y-1/2 lg:-translate-y-0 lg:translate-x-0  lg:top-[4%] right-1/2 translate-x-1/2 lg:right-[20%] bg-white shadow-lg p-[18px] lg:p-[30px] z-10 text-center ">
+        <h2 className="lg:text-[48px] md:text-[32px] text-[26px] font-normal text-center font-marcellus text-lagoBlack leading-normal lg:leading-[56px] lg:mt-2 w-full capsizedText2 whitespace-nowrap mb-[15px] lg:mb-[25px]">
           Feel Free to <br /> Drop Us a Message
         </h2>
-        <p className="text-lagoGray text-[16px] font-normal font-jost leading-[26px] w-full mb-8">
-          Do you have anything in your brain to let us know? Kindly <br /> don't fail
+        <p className="text-lagoGray text-[14px] lg:text-[16px] font-normal font-jost leading-[18px] lg:leading-[26px] w-[100%] lg:w-[95%] capsizedText4 mb-[15px] md:mb-[25px]">
+          Do you have anything in your brain to let us know? Kindly don't fail
           to connect with us through our contact form.
         </p>
         {/* Yatay çizgi */}
-        <hr className="mb-8 border-black w-1/2 mx-auto" />
+        <hr className=" border-black w-1/2 mx-auto mb-[20px] md:mb-[25px]" />
 
-        <form className="flex flex-col w-full">
+        <form className="flex flex-col w-full h-auto">
           {/* Full Name */}
-          <div className="mb-2">
+          <div className="mb-[10px] lg:mb-[17px]">
             <input
               id="fullName"
               type="text"
               placeholder="Your Full Name"
-              className="block w-full border border-gray-300 rounded-md px-3 py-2"
+              className="block w-full border border-gray-300 px-3 py-2 lg:px-[24px] lg:py-[18px] placeholder:text-[14px] placeholder:lg:text-[16px]"
             />
           </div>
           {/* Email Address */}
-          <div className="mb-2">
+          <div className="mb-[10px] lg:mb-[17px]">
             <input
               id="email"
               type="email"
               placeholder="Your Email Address"
-              className="block w-full border border-gray-300 rounded-md px-3 py-2"
+              className="block w-full border border-gray-300 px-3 py-2 lg:px-[24px] lg:py-[18px] placeholder:text-[14px] placeholder:lg:text-[16px]"
             />
           </div>
           {/* Your Message */}
-          <div className="mb-2">
+          <div className="">
             <textarea
               id="message"
               rows="4"
               placeholder="Your Message"
-              className="block w-full border border-gray-300 rounded-md px-3 py-2"
+              className="block w-full border border-gray-300  px-3 py-2 lg:px-[24px] lg:py-[18px] placeholder:text-[14px] placeholder:lg:text-[16px] max-h-[137px]"
             ></textarea>
           </div>
           {/* Checkbox */}
-          <div className="mb-4 flex items-start">
+          <div className=" flex items-start">
             <input
               id="saveInfo"
               type="checkbox"
               className="h-4 w-4 border border-gray-300 rounded mr-2 mt-4"
             />
-            <label htmlFor="saveInfo" className="text-sm text-gray-600 mt-3">
+            <label htmlFor="saveInfo" className="text-[12px] lg:text-[14px] text-gray-600 mt-[18px] lg:mt-[24px]">
               Save my name, email, and website in this browser for the next time I comment.
             </label>
           </div>
@@ -71,18 +73,23 @@ const Connect2 = () => {
           <button
             type="submit"
             className="
-              w-1/2
+              md:w-1/2
               mx-auto
               border
-              border-black
-              text-black
+              border-[#472C10]
               bg-transparent
               px-6
               py-3
-              mt-12
+              lg:mt-[25px]
               hover:bg-black
               hover:text-white
               transition-colors
+              text-[14px] lg:text-[16px]
+              uppercase
+              leading-[18px] lg:leading-[26px]
+              font-jost text-lagoBlack whitespace-nowrap
+              w-auto items-center justify-center
+              
             "
           >
             SEND MESSAGE
