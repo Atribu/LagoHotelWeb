ec439509391ca2a5820df584f19a726de0cbca3a
Ece Bayramer <ecebayramer20@gmail.com>
2025-04-21 10:56:04 +0300

    edited

--- client/app/[locale]/beachpools/Components/Beach4.jsx
+++ client/app/[locale]/beachpools/Components/Beach4.jsx
@@ -20,7 +20,7 @@ const Beach4 = () => {
                 className="absolute top-0 left-0 w-full h-full object-cover object-center"
             >
                 <source 
-                src="https://s3-figma-videos-production-sig.figma.com/video/TEAM/1188089547498931215/cb805ea008b01e5fb54de3eb7657cf110c9121fe?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=P1BTehC6-gMsVH8zvRkPg5OpmsWUmRzZxTb7Tv3KX7GUDhJlho~dQsaA2Zzjy6wO~1C3T1Q~6~H~FB78GU2OtmNXb03OWIww2KUOOZpNEhWTQOZk-s64RKPnqlz3q~ma-wWvsElpjvNuVKvSmFn9ZmXRsvz895AYN7zfYG9bVcySYbAEOocwlo1CIj-rWwPSZUWiRoyvVHgDjL1QJSQMZpbKOG-BrDC-IwLoTCjuBAHJ07XQbZtUIK806m4V7co5Pu-f~XU6BadzNQh~GrgnOB3IKj0bL4bvvbGzZFTYJPiEP4dmo5LxWmZI0oVvY0-wPDn4hGBPJcbexP~8QSLX9w__"
+                src="/audio/beachGif.mp4"
                 type="video/mp4"
                 />
                 Tarayıcınız bu videoyu desteklemiyor.
